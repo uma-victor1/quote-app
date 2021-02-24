0483cbd5468769c044a4c12b1d8f470dd43b429a
uma victor <umavictor11@gmail.com>
2021-02-24 15:38:31 +0100

    connected database

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,20 @@ const bodyParser = require("body-parser")
 // create our express app
 const app = express()
 
+// database
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://uma-victor1:jhon3165869@quote-app.ba0sr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+// middleware
+app.use(bodyParser.json())
+
 // routes
 app.get("/", (req,res)=>{
     res.send("my home page dey show sha")
