b036c17bbe37b1a6e26019725f681edf5dfcd030
uma-victor1 <umavictor11@gmail.com>
2021-04-17 10:24:42 +0100

    set up all routes for our app

--- backend/routes/Quotes.js
+++ backend/routes/Quotes.js
@@ -1,18 +1,51 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const Quote = require('../models/Quotes');
 
+// Get all routes
+router.get('/', async (req, res) => {
+	const quotes = await Quote.find();
 
-// all routes
-router.get('/', (req,res)=>{
-    res.send("Our quotes route")
-})
+	res.json(quotes);
+});
 
-// new post route
-router.post('/new', (req,res)=>{
-    res.send('posted info')
-})
+// Create new quote
+router.post('/new', async (req, res) => {
+	const newQuote = new Quote(req.body);
+	
+	const savedQuote = await newQuote.save();
 
+	res.json(savedQuote);
+});
 
+// Get specific quote
+router.get('/get/:id', async (req, res) => {
+	const q = await Quote.findById({ _id: req.params.id });
 
+	res.json(q);
+});
 
-module.exports = router
+// Delete a quote
+router.delete('/delete/:id', async (req, res) => {
+	const result = await Quote.findByIdAndDelete({ _id: req.params.id });
+
+	res.json(result);
+});
+
+// Update a quote
+router.patch('/update/:id', async (req, res) => {
+	const q = await Quote.updateOne({_id: req.params.id}, {$set: req.body});
+
+	res.json(q);
+});
+
+// Get random quote
+router.get('/random', async (req, res) => {
+	const count = await Quote.countDocuments();
+	const random = Math.floor(Math.random() * count);
+	const q = await Quote.findOne().skip(random);
+
+	res.json(q);
+});
+
+module.exports = router;
